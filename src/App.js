13f88a4de101b8a24251bc905b3714d116317670
pierrevaut <p.vautherin@hotmail.com>
2020-02-07 23:20:50 +0100

    [ADD]  Call to API using hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const MyComponent = () => <p>Hello, I am React component!</p>
+
+
 function App() {
+  const [hasError, setErrors] = useState(false);
+  const [data, setData] = useState([{url: ""}]);
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch("https://api.thecatapi.com/v1/images/search/");
+      res
+        .json()
+        .then(res => setData(res))
+        .catch(err => setErrors(err));
+    }
+    fetchData();
+  }, []);
+
+
   return (
+    <>
+    {hasError ?
+    <p>Error</p> 
+    :(
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={data[0].url} className="App-logo" alt="" />
+        <MyComponent />
       </header>
     </div>
+  )}
+  </>
   );
 }
 
 export default App;
+
